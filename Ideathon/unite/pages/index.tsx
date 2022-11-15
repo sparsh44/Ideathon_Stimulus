a7aebae1f9c4310eecfcfbddcafc8cb8e5509086
sparsh44 <sparshsinghbhatia339@gmail.com>
2022-11-15 10:32:42 +0530

    Override css on index.tsx

--- Ideathon/unite/pages/index.tsx
+++ Ideathon/unite/pages/index.tsx
@@ -5,15 +5,28 @@ import Account from '../components/Account'
 import { useState } from 'react'
 import HomePage from '../components/HomeScreen/HomePage'
 
+const customTheme = {
+  default: {
+    colors: {
+      brand: 'hsl(153 60.0% 53.0%)',
+      brandAccent: 'hsl(154 54.8% 45.1%)',
+      brandButtonText: 'white',
+      // ..
+  },
+}
+}
 const Home = () => {
   var [pagechanger, setpage] = useState("account")
   const session = useSession()
   const supabase = useSupabaseClient()
 
+  
   return (
+    
     <div>
       {!session ? (
-         <Auth supabaseClient={supabase} />
+         <Auth supabaseClient={supabase} 
+         appearance={{ theme: customTheme }} theme="default"/>
       ) : (
         // if(pagechanger==="account"){
         // <Account session={session}/>
@@ -22,6 +35,7 @@ const Home = () => {
        
       )}
     </div>
+
 
   )
 }
