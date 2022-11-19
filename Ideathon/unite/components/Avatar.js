1ee41b2e6cba5b49c786c1ebabd1b34bc48aff51
Modernbeast02 <89138051+Modernbeast02@users.noreply.github.com>
2022-11-19 18:08:33 +0530

    Ankur- Profile Pic Circle

--- Ideathon/unite/components/Avatar.js
+++ Ideathon/unite/components/Avatar.js
@@ -59,7 +59,7 @@ export default function Avatar({ uid, url, size, onUpload }) {
         <img
           src={avatarUrl}
           alt="Avatar"
-          className="avatar image"
+          className="avatar image rounded-full -ml-12"
           style={{ height: size, width: size }}
         />
       ) : (
