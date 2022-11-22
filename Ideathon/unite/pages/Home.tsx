47582931c7599c9cc182f1d27c453b9cd44d58dd
Gopal Verma <vgopaldps@gmail.com>
2022-11-22 11:45:47 +0530

    navbar made responsive

--- Ideathon/unite/pages/Home.tsx
+++ Ideathon/unite/pages/Home.tsx
@@ -132,7 +132,7 @@ const Home: NextPage = () => {
                 </div>
                 <div className='flex my-7 mx-auto max-w-5xl'>
                     <Feed />
-                    <div className='sticky top-20 mt-5 mx-5 ml-5 hidden h-fit min-w-[300px] rounded-md border border-grap-300 bg-white lg:inline'>
+                    <div className='sticky top-5 mt-5 mx-5 ml-5 hidden h-fit min-w-[300px] rounded-md border border-grap-300 bg-white lg:inline'>
                         <p className='text-md mb-1 p-4 pb-3 font-bold '>All Communities</p>
                         <COmmunity />
                     </div>
