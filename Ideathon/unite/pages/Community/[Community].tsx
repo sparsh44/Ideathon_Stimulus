f4cff734859151ed1d5442d0c58a09e770bae46a
Gopal Verma <vgopaldps@gmail.com>
2022-11-23 16:30:30 +0530

    added react routing

--- Ideathon/unite/pages/Community/[Community].tsx
+++ Ideathon/unite/pages/Community/[Community].tsx
@@ -9,19 +9,13 @@ import PostData from '../../assets/PostData';
 import { useSupabaseClient } from '@supabase/auth-helpers-react';
 
 function Community() {
-
-
+    const { query } = useRouter()
 
     useEffect(() => {
         allPost();
-    })
+    }, [])
 
     const [posts, setPost] = useState([])
-
-//  following query to fetch community using next router
-    // const {
-    //     query: { community },
-    // } = useRouter();
 
     const supabase = useSupabaseClient()
     const allPost = async () => {
@@ -32,26 +26,28 @@ function Community() {
 
 
     return (
-        <div className={` h-36 bg-red-400 p-8`}>
+        <div className={` h-36 bg-red-400 pt-8`}>
             <div className='-mx-8 mt-28 bg-white'>
                 <div className='mx-auto flex max-w-5xl items-center space-x-4 pb-5'>
-                    <div className='-mt-5 ml-0'>
+                    <div className='-mt-5 ml-10'>
                         <CommunityAvatar />
                     </div>
                     <div className='py-2'>
                         <h1 className='text-3xl font-semibold'>
-                            Welcome to the {CommunityData[0].title}
+                            Welcome to the {query.Community}
                         </h1>
-                        <p className='text-sm text-gray-400'>{CommunityData[0].title}</p>
+                        <p className='text-sm text-gray-400'>{query.Community}</p>
                     </div>
                 </div>
             </div>
-            <div className='mx-auto mt-5 max-w-5xl pb-10'>
-                <div className='flex my-7 mx-auto max-w-5xl'>
-                    <PostBox community={"ACM CSS"} />
+            <div className=''>
+                <div className='sticky top-0 z-10 flex my-7 mx-auto max-w-5xl'>
+                    <PostBox community={query.Community} />
                     {/* <PostBox community={community as string} /> */}
                 </div>
-                <Feed posts={posts} />
+                <div className='flex my-7 mx-auto max-w-5xl'>
+                    <Feed posts={posts} />
+                </div>
             </div>
         </div>
     )
