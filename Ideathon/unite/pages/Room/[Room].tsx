59c4fe49f90735cc030a30c97c5a07b3bd32ef43
Gopal Verma <vgopaldps@gmail.com>
2022-11-29 19:11:45 +0530

    updated room.tsx

--- Ideathon/unite/pages/Room/[Room].tsx
+++ Ideathon/unite/pages/Room/[Room].tsx
@@ -3,6 +3,7 @@ import Sidebar from '../../components/Sidebar'
 import ChatNavbar from '../../components/ChatNavbar'
 import ChatBox from '../../components/ChatBox'
 import ChatInput from '../../components/ChatInput'
+import { PaperAirplaneIcon } from '@heroicons/react/solid'
 
 function Room() {
     return (
@@ -16,7 +17,19 @@ function Room() {
             </div>
             <div className=' w-screen'>
             </div>
-            <ChatInput />
+            <form className='fixed -bottom-5 z-50 w-full  flex py-5 border-1 border-gray-100'>
+                <input
+                    type="text"
+                    className='flex-1  border-gray-300 focus:outline-none px-5 py-2 disabled:opacity-50 disabled:cursor-not-allowed'
+                    placeholder='Enter message here...'
+                />
+                <button
+                    type="submit"
+                    className='bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4  disabled:opacity-50 disabled:cursor-not-allowed'
+                >
+                    <PaperAirplaneIcon className='w-6 h-6' />
+                </button>
+            </form>
         </div>
     )
 }
