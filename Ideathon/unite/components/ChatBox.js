dade78441f424e926c6121c01f02e78f57c1c6f1
Gopal Verma <vgopaldps@gmail.com>
2022-11-30 13:23:55 +0530

    chat room css readded

--- Ideathon/unite/components/ChatBox.js
+++ Ideathon/unite/components/ChatBox.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ChatInput from './ChatInput'
+import Image from 'next/image';
 
 
 function ChatBox(props) {
@@ -8,27 +9,74 @@ function ChatBox(props) {
     var arr = chats || [];
     arr.forEach(chat => {
         rows.push(
-            <div>{chat.profile_id===props.user.id?(<div className='m-2 flex-1 bg-blue-50 outline-none p-2'>
-                {chat.content}
-                {
-                    chat.profile.username
-                }
-            </div>):(<div >
-                {chat.content}
-                {
-                    chat.profile.username
-                }
-            </div>)}
-            </div>
+
+            // <div>{chat.profile_id === props.user.id ? (
+            //     <div className='my-10 flex rounded text-white bg-blue-500 outline-none p-2'>
+            //         {chat.content}
+            //         {chat.profile.username}
+            //     </div>
+            // ) : (
+            //     <div className='my-10 flex-1 text-white rounded bg-red-500 outline-none p-2'>
+            //         {chat.content}
+            //         {chat.profile.username}
+            //     </div>
+            // )}
+            // </div>
+
+
+            (chat.profile_id === props.user.id) ? (
+                <div className='flex w-fit ml-auto'>
+                    <div className='flex-shrink-0 order-2'>
+                        <Image
+                            className="rounded-full mx-2"
+                            height={30}
+                            width={30}
+                            src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
+                            alt="Profile Picture"
+                        />
+                    </div>
+                    <div className=' ml-auto'   >
+                        <p className='text-[0.65rem] px-[2px] pb-[2px] text-blue-400'>{chat.profile.username}</p>
+                        <div className=' items-end'>
+                            <div className='px-3 py-2 rounded-lg w-fit text-white bg-blue-400'>
+                                <p>{chat.content}</p>
+                            </div>
+                            <p className='text-[0.65rem] italic  px-[2px] pb-[2px] text-gray-400'>30/11/2022 12:15 PM</p>
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                <div className='flex w-fit'>
+                    <div className='flex-shrink-0'>
+                        <Image
+                            className="rounded-full mx-2"
+                            height={30}
+                            width={30}
+                            src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
+                            alt="Profile Picture"
+                        />
+                    </div>
+                    <div>
+                        <p className='text-[0.65rem] px-[2px] pb-[2px] text-red-400'>{chat.profile.username}</p>
+                        <div className=' items-end'>
+                            <div className='px-3 py-2 rounded-lg w-fit text-white bg-red-400'>
+                                <p>{chat.content}</p>
+                            </div>
+                            <p className='text-[0.65rem] italic  px-[2px] pb-[2px] text-gray-400'>30/11/2022 12:15 PM</p>
+                        </div>
+                    </div>
+                </div>)
+
+
+
+
+
+
         )
     });
     return (
-        <div>
-            <div className="p-2">
-                {rows}
-            </div>
-
-
+        <div className=' w-full'>
+            {rows}
         </div>
     )
 }
--- Ideathon/unite/pages/Room/[Room].tsx
+++ Ideathon/unite/pages/Room/[Room].tsx
@@ -5,94 +5,89 @@ import ChatBox from '../../components/ChatBox'
 import ChatInput from '../../components/ChatInput'
 
 import { PaperAirplaneIcon } from '@heroicons/react/solid'
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
-import { useSupabaseClient,useUser,useSession } from '@supabase/auth-helpers-react'
+import { useSupabaseClient, useUser, useSession } from '@supabase/auth-helpers-react'
 
 function Room() {
-    const user=useUser()
-    const session=useSession();
-    const supabase=useSupabaseClient();
-    const router=useRouter();
-    const [messages,setMessage]=useState([]);
-    const [content,usecont]=useState("");
-    const[content2,useCont]=useState({});
-    
-    useEffect(()=>{
-        if(!router.isReady) return
-    
+    const user = useUser()
+    const session = useSession();
+    const supabase = useSupabaseClient();
+    const router = useRouter();
+    const [messages, setMessage] = useState([]);
+    const [content, usecont] = useState("");
+    const [content2, useCont] = useState({});
+
+    useEffect(() => {
+        if (!router.isReady) return
+
         message();
-     
+
         // console.log(router.query);
         // console.log(user);
-        
-    },[router.isReady])
-    const message=async()=>{
-        let {data,error}=await supabase.from('messages').select("*, profile: profiles(id, username)").eq("room_name",String(router.query.Room));
-        if(error){
+
+    }, [router.isReady])
+    const message = async () => {
+        let { data, error } = await supabase.from('messages').select("*, profile: profiles(id, username)").eq("room_name", String(router.query.Room));
+        if (error) {
             throw error;
         }
-      
+
         console.log("Why")
         console.log(data);
-        var arr=data||[];
-        for ( var i=0;i<arr.length;i++){
+        var arr = data || [];
+        for (var i = 0; i < arr.length; i++) {
             messages.push(arr[i]);
         }
 
-    } 
+    }
 
-    const InsertMessage=async()=>{
-        var message={
-            profile_id:user.id,
-            content:content,
-            room_name:router.query.Room,
+    const InsertMessage = async () => {
+        var message = {
+            profile_id: user.id,
+            content: content,
+            room_name: router.query.Room,
         }
-        let {error}=await supabase.from('messages').insert([message])
-       if(error) {
-        throw error
-       }
-     
-      
-    
-   
+        let { error } = await supabase.from('messages').insert([message])
+        if (error) {
+            throw error
+        }
+
+
+
+
     }
     supabase.channel('public:messages').on('postgres_changes', { event: 'INSERT', schema: 'public', table: 'messages' }, payload => {
-       
+
         console.log(payload);
         console.log("YES")
-       setMessage((messages)=>[].concat(messages,[payload.new]));
-     
-        
+        setMessage((messages) => [].concat(messages, [payload.new]));
 
-}).subscribe()
-console.log(messages);
+
+
+    }).subscribe()
+    console.log(messages);
 
     return (
-        
         <div className=' w-full'>
-        <ChatNavbar room={router.query.Room} />
-        <div className='flex'>
-            <Sidebar />
-            <div className='ml-1 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto  pb-10 flex justify-start w-4/5'>
-                <ChatBox allMessages = {messages} user={user}/>
+            <ChatNavbar room={router.query.Room} />
+            <div className='flex'>
+                <Sidebar />
+                <div className='mx-10 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto  pb-10 flex justify-center w-screen'>
+                    <ChatBox allMessages={messages} user={user} />
+                </div>
+            </div>
+            <div className=' w-full'>
+                <input
+                    type="text"
+                    value={content || ""}
+                    onChange={(e) => { usecont(e.target.value) }}
+                    className='flex-1  border-gray-300 focus:outline-none px-5 py-2 disabled:opacity-50 disabled:cursor-not-allowed'
+                    placeholder='Enter message here...'
+                />
+                <PaperAirplaneIcon className='w-6 h-6' onClick={() => { InsertMessage() }} />
             </div>
         </div>
-        <div className=' w-screen'>
-        </div>
-       
-            <input
-                type="text"
-                value={content|| ""}
-                onChange={(e)=>{usecont(e.target.value)}}
-                className='flex-1  border-gray-300 focus:outline-none px-5 py-2 disabled:opacity-50 disabled:cursor-not-allowed'
-                placeholder='Enter message here...'
-            />
-           
-                <PaperAirplaneIcon className='w-6 h-6' onClick={()=>{InsertMessage()}} />
-    
-        
-    </div>
     )
 }
 
