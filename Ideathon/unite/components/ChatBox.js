46c27c4ea8662b55e7c6a8005832d2b96d6d92e0
Gopal Verma <vgopaldps@gmail.com>
2022-11-30 18:26:56 +0530

    saved chatbox.js

--- Ideathon/unite/components/ChatBox.js
+++ Ideathon/unite/components/ChatBox.js
@@ -13,14 +13,14 @@ function ChatBox(props) {
 
 
             (chat.profile_id === props.user) ? (
-                <div className='flex w-fit ml-auto'>
+                <div className='flex w-fit ml-auto order-2'>
                     <div className='flex-shrink-0 order-2'>
                         <MyAvatar avatar_url={chat.avatar_url} />
                     </div>
                     <div className=' ml-auto'   >
                         <p className='text-[0.65rem] px-[2px] pb-[2px] text-blue-400'>{chat.username}</p>
-                        <div className=' items-end'>
-                            <div className='px-3 py-2 rounded-lg w-fit text-white bg-blue-400'>
+                        <div className=' items-end order-2'>
+                            <div className='px-3 py-2 rounded-lg w-fit text-white bg-blue-400 order-2'>
                                 <p>{chat.content}</p>
                             </div>
                             <p className='text-[0.65rem] italic  px-[2px] pb-[2px] text-gray-400'>30/11/2022 12:15 PM</p>
